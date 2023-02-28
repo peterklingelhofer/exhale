97fc6b83d1be49526333d70bcf86e49257c0c90c
Peter Klingelhofer <peterklingelhofer@gmail.com>
2023-02-27 21:02:24 -0600

    feat: sinusoidal animation speed calculation

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import math
 import time
 import tkinter as tk
 
 # set tint color
-tint_color = "#F4DFC9"
+tint_color = "#000"
+use_frame_rate = 30
 
 # get screen dimensions
 root = tk.Tk()
@@ -34,24 +36,31 @@ def animate():
     # calculate height of line
     line_height = screen_height // 2
 
+    # calculate the number of frames to use for each phase of the animation
+    frames_up = math.ceil(duration_up * use_frame_rate)
+    frames_down = math.ceil(duration_down * use_frame_rate)
+
+    # calculate the increment for each frame
+    increment_up = (math.pi / 2) / frames_up
+    increment_down = (math.pi / 2) / frames_down
+
     # animate line moving up and down
     while True:
-        for i in range(0, screen_height):
-            canvas.coords(tinted_rect, 0, screen_height - i, screen_width, screen_height)
+        for i in range(frames_up):
+            sin_value = math.sin(increment_up * i)
+            height = screen_height - (sin_value * screen_height)
+            canvas.coords(tinted_rect, 0, height, screen_width, screen_height)
             overlay.update()
-            time.sleep(duration_up / screen_height)
-        for i in range(screen_height - 1, line_height - 1, -1):
-            canvas.coords(tinted_rect, 0, screen_height - i, screen_width, screen_height)
+            time.sleep(duration_up / frames_up)
+        for i in range(frames_down):
+            sin_value = math.sin(increment_down * i)
+            height = (sin_value * screen_height)
+            canvas.coords(tinted_rect, 0, height, screen_width, screen_height)
             overlay.update()
-            time.sleep(duration_down / (screen_height - line_height))
-        for i in range(line_height - 1, -1, -1):
-            canvas.coords(tinted_rect, 0, screen_height - i, screen_width, screen_height)
-            overlay.update()
-            time.sleep(duration_up / line_height)
+            time.sleep(duration_down / frames_down)
 
 
-
-# Press the green button in the gutter to run the script.
+# press the green button in the gutter to run the script
 if __name__ == '__main__':
     # start animation
     animate()
