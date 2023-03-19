264b0e166bec67eaa0b2dd49d19721bd50fff729
Peter Klingelhofer <peterklingelhofer@gmail.com>
2023-03-19 08:43:18 -0500

    chore: settings more DRY

--- exhale/exhale/SettingsView.swift
+++ exhale/exhale/SettingsView.swift
@@ -1,6 +1,32 @@
 //  SettingsView.swift
 import SwiftUI
 
+struct TextFieldWithValidation: View {
+    var title: String
+    @Binding var value: Double
+    var formatter: NumberFormatter
+    var minimumValue: Double
+
+    var body: some View {
+        HStack {
+            Text(title)
+            Spacer()
+            TextField("", value: $value, formatter: formatter)
+                .onChange(of: value) { newValue in
+                    if value < minimumValue {
+                        value = minimumValue
+                    }
+                    if let maximum = formatter.maximum?.doubleValue,
+                       value > maximum {
+                        value = maximum
+                    }
+                }
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .frame(width: 100)
+        }
+    }
+}
+
 struct SettingsView: View {
     @Binding var showSettings: Bool
     @Binding var overlayColor: Color
@@ -12,16 +38,6 @@ struct SettingsView: View {
     @Binding var drift: Double
     @Binding var overlayOpacity: Double
     
-    let positiveNumberFormatter: NumberFormatter = {
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .decimal
-        formatter.maximumFractionDigits = 2
-        formatter.minimum = 0
-        formatter.maximum = 1
-        formatter.usesGroupingSeparator = false
-        return formatter
-    }()
-    
     func createNumberFormatter(minimumValue: Double, maximumValue: Double? = nil) -> NumberFormatter {
         let formatter = NumberFormatter()
         formatter.numberStyle = .decimal
@@ -32,16 +48,6 @@ struct SettingsView: View {
         }
         formatter.usesGroupingSeparator = false
         return formatter
-    }
-    
-    private func validate(value: inout Double, formatter: NumberFormatter, minimumValue: Double) {
-        if value < minimumValue {
-            value = minimumValue
-        }
-        if let maximum = formatter.maximum?.doubleValue,
-           value > maximum {
-            value = maximum
-        }
     }
     
     var body: some View {
@@ -58,72 +64,17 @@ struct SettingsView: View {
                             
                             ColorPicker("Background Color", selection: $backgroundColor, supportsOpacity: true)
                             
-                            HStack {
-                                Text("Inhale Duration (s)")
-                                Spacer()
-                                TextField("", value: $inhaleDuration, formatter: createNumberFormatter(minimumValue: 0.5))
-                                    .onChange(of: inhaleDuration) { newValue in
-                                        validate(value: &inhaleDuration, formatter: createNumberFormatter(minimumValue: 0.5), minimumValue: 0.5)
-                                    }
-                                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                                    .frame(width: 100)
-                            }
+                            TextFieldWithValidation(title: "Inhale Duration (s)", value: $inhaleDuration, formatter: createNumberFormatter(minimumValue: 0.5), minimumValue: 0.5)
                             
-                            HStack {
-                                Text("Post-Inhale Hold (s)")
-                                Spacer()
-                                TextField("", value: $postInhaleHoldDuration, formatter: positiveNumberFormatter)
-                                    .onChange(of: postInhaleHoldDuration) { newValue in
-                                        validate(value: &postInhaleHoldDuration, formatter: positiveNumberFormatter, minimumValue: 0)
-                                    }
-                                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                                    .frame(width: 100)
-                            }
+                            TextFieldWithValidation(title: "Post-Inhale Hold (s)", value: $postInhaleHoldDuration, formatter: createNumberFormatter(minimumValue: 0), minimumValue: 0)
                             
-                            HStack {
-                                Text("Exhale Duration (s)")
-                                Spacer()
-                                TextField("", value: $exhaleDuration, formatter: createNumberFormatter(minimumValue: 0.5))
-                                    .onChange(of: exhaleDuration) { newValue in
-                                        validate(value: &exhaleDuration, formatter: createNumberFormatter(minimumValue: 0.5), minimumValue: 0.5)
-                                    }
-                                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                                    .frame(width: 100)
-                            }
+                            TextFieldWithValidation(title: "Exhale Duration (s)", value: $exhaleDuration, formatter: createNumberFormatter(minimumValue: 0.5), minimumValue: 0.5)
                             
-                            HStack {
-                                Text("Post-Exhale Hold (s)")
-                                Spacer()
-                                TextField("", value: $postExhaleHoldDuration, formatter: positiveNumberFormatter)
-                                    .onChange(of: postExhaleHoldDuration) { newValue in
-                                        validate(value: &postExhaleHoldDuration, formatter: positiveNumberFormatter, minimumValue: 0)
-                                    }
-                                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                                    .frame(width: 100)
-                            }
+                            TextFieldWithValidation(title: "Post-Exhale Hold (s)", value: $postExhaleHoldDuration, formatter: createNumberFormatter(minimumValue: 0), minimumValue: 0)
                             
+                            TextFieldWithValidation(title: "Drift", value: $drift, formatter: createNumberFormatter(minimumValue: 0.5), minimumValue: 0.5)
                             
-                            HStack {
-                                Text("Drift")
-                                Spacer()
-                                TextField("", value: $drift, formatter: createNumberFormatter(minimumValue: 0.5))
-                                    .onChange(of: drift) { newValue in
-                                        validate(value: &drift, formatter: createNumberFormatter(minimumValue: 0.5), minimumValue: 0.5)
-                                    }
-                                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                                    .frame(width: 100)
-                            }
-                            
-                            HStack {
-                                Text("Overlay Opacity")
-                                Spacer()
-                                TextField("", value: $overlayOpacity, formatter: createNumberFormatter(minimumValue: 0, maximumValue: 1))
-                                    .onChange(of: overlayOpacity) { newValue in
-                                        validate(value: &overlayOpacity, formatter: createNumberFormatter(minimumValue: 0, maximumValue: 1), minimumValue: 0)
-                                    }
-                                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                                    .frame(width: 100)
-                            }
+                            TextFieldWithValidation(title: "Overlay Opacity", value: $overlayOpacity, formatter: createNumberFormatter(minimumValue: 0, maximumValue: 1), minimumValue: 0)
                         }
                         .foregroundColor(.white)
                         .padding(.horizontal)
