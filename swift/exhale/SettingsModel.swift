d83815f33683c42b4ac5467fc2d6e987e6ea024c
Peter Klingelhofer <peterklingelhofer@gmail.com>
2023-04-13 19:18:53 -0500

    feat: [swift] fullscreen mode is default

--- swift/exhale/SettingsModel.swift
+++ swift/exhale/SettingsModel.swift
@@ -5,14 +5,14 @@ class SettingsModel: ObservableObject {
     @Published var backgroundColor: Color = Color.black
     @Published var inhaleColor: Color = Color(red: 1, green: 0, blue: 0)
     @Published var exhaleColor: Color = Color(red: 0, green: 0, blue: 1)
-    @Published var colorFillType: ColorFillType = .linear
+    @Published var colorFillType: ColorFillType = .constant
     @Published var inhaleDuration: TimeInterval = 5
     @Published var postInhaleHoldDuration: TimeInterval = 0
     @Published var exhaleDuration: TimeInterval = 10
     @Published var postExhaleHoldDuration: TimeInterval = 0
     @Published var drift: Double = 1.0
     @Published var overlayOpacity: Double = 0.1
-    @Published var shape: AnimationShape = .rectangle
+    @Published var shape: AnimationShape = .fullscreen
     @Published var animationMode: AnimationMode = .sinusoidal
-    @Published var colorTransitionEnabled: Bool = false
+    @Published var colorTransitionEnabled: Bool = true
 }
