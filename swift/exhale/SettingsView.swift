a1cc15084db44920fe473904f908284266a8994b
Peter Klingelhofer <peterklingelhofer@gmail.com>
2023-08-12 13:04:36 -0500

    feat: Disable Gradient Type & Background Color for full screen, as they have no effect

--- swift/exhale/SettingsView.swift
+++ swift/exhale/SettingsView.swift
@@ -103,21 +103,8 @@ struct SettingsView: View {
                                     ColorPicker("", selection: $backgroundColor, supportsOpacity: false)
                                         .labelsHidden()
                                         .frame(alignment: .trailing)
+                                        .disabled(shape == .fullscreen)
                                 }.help("Choose the background color, or the color outside of the animation shape. This parameter has no effect if the Shape parameter is set to Fullscreen.")
-                                
-                                HStack {
-                                    Text("Gradient Type")
-                                        .frame(width: labelWidth, alignment: .leading)
-                                    
-                                    Picker("", selection: $colorFillType) {
-                                        ForEach(ColorFillType.allCases) { type in
-                                            Text(type.rawValue).tag(type)
-                                        }
-                                    }
-                                    .pickerStyle(MenuPickerStyle())
-                                    .frame(width: controlWidth)
-                                    .labelsHidden()
-                                }.help("Choose the gradient color effect. Constant allows the change the color to transition over time between the Inhale Color and Exhale Color and back. Linear Gradient causes abrupt color transitions at the end of the inhale and exhale phases (which can make it easier to notice when it is time to reverse the direction of your breathing), and enables a color gradient from the Background Color to the Inhale Color or Exhale Color (depending on the current phase). When the Shape is Circle the Linear Gradient color transition is from the innermost center point of the Circle to the diameter, whereas with the Rectangle shape the Linear Gradient color transition is from the bottom of the Rectangle to the top. This parameter has no effect if the Shape parameter is set to Fullscreen.")
                                 
                                 HStack {
                                     Text("Shape")
@@ -133,6 +120,21 @@ struct SettingsView: View {
                                     .labelsHidden()
                                 }.help("Choose the Shape of the animation. Fullscreen changes the color of every pixel on the screen, starting with the Inhale Color at the beginning of the inhale phase and transitioning to the Exhale Color, then for the exhale phase transitioning back from the Exhale Color to the Inhale Color (Fullscreen uses Gradient Type Constant, setting it to Linear Gradient has no effect). Rectangle rises vertically from the bottom of the screen to the top for the inhale phase, and then lowers back down from the top to the bottom for the exhale phase. Circle grows outwards starting from a single point in the center of the screen to the outer edges of the screen for the inhale phase, and then shrinks back to the center again for the exhale phase.")
                                 
+                                HStack {
+                                    Text("Gradient Type")
+                                        .frame(width: labelWidth, alignment: .leading)
+                                    
+                                    Picker("", selection: $colorFillType) {
+                                        ForEach(ColorFillType.allCases) { type in
+                                            Text(type.rawValue).tag(type)
+                                        }
+                                    }
+                                    .pickerStyle(MenuPickerStyle())
+                                    .frame(width: controlWidth)
+                                    .disabled(shape == .fullscreen)
+                                    .labelsHidden()
+                                }.help("Choose the gradient color effect. Constant allows the change the color to transition over time between the Inhale Color and Exhale Color and back. Linear Gradient causes abrupt color transitions at the end of the inhale and exhale phases (which can make it easier to notice when it is time to reverse the direction of your breathing), and enables a color gradient from the Background Color to the Inhale Color or Exhale Color (depending on the current phase). When the Shape is Circle the Linear Gradient color transition is from the innermost center point of the Circle to the diameter, whereas with the Rectangle shape the Linear Gradient color transition is from the bottom of the Rectangle to the top. This parameter has no effect if the Shape parameter is set to Fullscreen.")
+
                                 HStack {
                                     Text("Animation Mode")
                                         .frame(width: labelWidth, alignment: .leading)
