54e3c693ec9097b9fa1a325604356fa7852c3cc2
Peter Klingelhofer <>
2026-04-20 20:07:12 -0500

    ci(snap): pip-install snapcraft for upload to dodge bundled pymacaroons bug

--- rust/scripts/bundle-appimage.sh
+++ rust/scripts/bundle-appimage.sh
@@ -30,7 +30,7 @@ APPDIR="$OUT_DIR/exhale.AppDir"
 TOOL_DIR="$OUT_DIR/bin"
 APPIMAGETOOL="$TOOL_DIR/appimagetool"
 
-VERSION="${VERSION:-2.0.12}"
+VERSION="${VERSION:-2.0.14}"
 OUT_APPIMAGE="$OUT_DIR/exhale-${VERSION}-x86_64.AppImage"
 
 # ── Helpers ──────────────────────────────────────────────────────────────────
